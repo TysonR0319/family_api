f45c359a9e61f72f8d74fc160be7c0ac7d6e837d
TysonR0319 <tysonr0319@gmail.com>
2022-08-13 14:28:03 +0000

    final commit

--- src/app.py
+++ src/app.py
@@ -26,17 +26,102 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
+    family = jackson_family.get_all_members()
+    return jsonify(family), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+#BY ID
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member),200
+    else:
+        return jsonify({
+            "fail": "id doesn't exist"
+        }), 400
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    id = request.json.get('id')
+    first_name = request.json.get('first_name')
+    last_name = "Jackson"
+    age = request.json.get('age')
+    lucky_numbers = request.json.get('lucky_numbers')
+
+    if not first_name:
+        return jsonify({
+            "message": "firstname required"
+        }), 400
+    if not last_name:
+        return jsonify({
+            "message": "Lastname required"
+        }), 400
+    if not age:
+        return jsonify({
+            "message": "Age required"
+        }), 400
+    if not lucky_numbers:
+        return jsonify({
+            "message": "Lucky number(s) is/are required"
+        }), 400
+        
+    member = {
+        'first_name': first_name,
+        'age': age,
+        'lucky_numbers': lucky_numbers,
+        'id': id
     }
 
+    jackson_family.add_member(member)
+    return jsonify({
+        "success": "Family Member created"
+        }), 200
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['PUT'])     
+def update_member(member_id):   
+
+    member = jackson_family.get.member(id)
+    first_name = request.json.get('first_name')
+    last_name = "Jackson"
+    age = request.json.get('age')
+    lucky_numbers = request.json.get('lucky_numbers')
+
+    if not first_name:
+        return jsonify({
+            "message": "firstname required"
+        }), 400
+    if not last_name:
+        return jsonify({
+            "message": "Lastname required"
+        }), 400
+    if not age:
+        return jsonify({
+            "message": "Age required"
+        }), 400
+    if not lucky_numbers:
+        return jsonify({
+            "message": "Lucky number(s) is/are required"
+         }), 400
+
+    member['first_name'] = first_name
+    member['last_name'] = last_name
+    member['age'] = age 
+    member['lucky_numbers'] = lucky_numbers
+
+    return jsonify({
+        "message": "Family member updated "
+    }), 200
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if not member:
+        return jsonify({
+            "error": "Member doesn't exist"
+        }), 400
+    return jsonify(member)
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
